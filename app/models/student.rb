9238b6630d7c231b44fe43fa06e666c2e1aac96a
Matthew <mattcorralejo1@gmail.com>
2020-05-23 10:58:58 -0700

    "commit'

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,3 +14,13 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 end
+
+def self.search(query)
+  if query.present?
+    where('NAME like ?', "%#{query}%")
+  else 
+    self.all 
+  end 
+  end 
+end 
+end
